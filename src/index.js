8fdce1a9604b236e5003fa317b7eb0949486a803
Austin Norgaard <austinnorgaard@gmail.com>
2023-03-18 01:28:31 -0700

    updated site macro details

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './components/home/home.js';
 import Project1 from './components/projects/project1';
-import Project2 from './components/projects/project2';
-import Project3 from './components/projects/project3';
 import Projects from './components/projects/projects-page.js';
 import ErrorPage from './components/error-page/error-page';
 import Info from './components/info-page/info-page';
@@ -20,23 +18,13 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
   },
   {
-    path: "/projects",
+    path: "/music",
     element: <Projects />,
     errorElement: <ErrorPage />,
   },
   {
-    path: "/projects/project1",
+    path: "/music/heartsmile",
     element: <Project1 />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/projects/project2",
-    element: <Project2 />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/projects/project3",
-    element: <Project3 />,
     errorElement: <ErrorPage />,
   },
   {
